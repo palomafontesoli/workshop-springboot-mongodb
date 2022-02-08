a107ba9bbbb25156a29e21e6c3b64b31988b40b6
Paloma Oliveira <pfoliveira207@gmail.com>
2022-02-08 17:08:17 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/workshopmongo/resources/exceptions/StandardError.java
@@ -0,0 +1,65 @@
+package com.workshopmongo.resources.exceptions;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long timestamp;
+	private Integer status;
+	private String Error;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+	}
+
+	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		Error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return Error;
+	}
+
+	public void setError(String error) {
+		Error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
