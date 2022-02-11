0756dc4385276407e49acee9b4029b298dcca847
Paloma Oliveira <pfoliveira207@gmail.com>
2022-02-11 03:32:31 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/workshopmongo/resources/UserResource.java
+++ src/main/java/com/workshopmongo/resources/UserResource.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -35,7 +35,7 @@ public class UserResource {
 	}
 	
 	@GetMapping
-	@RequestMapping(value = "/{id}")
+	@RequestMapping(value="/{id}")
 	public ResponseEntity <UserDTO> findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
@@ -50,10 +50,16 @@ public class UserResource {
 	}
 	
 	@DeleteMapping("/{id}")
-//	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity <Void> delete(@PathVariable String id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PutMapping("/{id}")
+	public ResponseEntity <Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj =service.update(obj);
+		return ResponseEntity.noContent().build();
+	}			
 }
